a0faf4a434c9a6ae0e8cc7112b1a15d920a13e07
lessmind <nevermind.cs96@gmail.com>
2012-01-15 17:28:04 +0800

    Modify `` to $() and $var to ${var} expression

--- install.sh
+++ install.sh
@@ -2,21 +2,21 @@
 
 # functions
 function lastPiece {
-	for piece in $( echo "$1" | tr " " "\n" ); do
-		last=$piece
+	for piece in $( echo ${1} | tr " " "\n" ); do
+		last=${piece}
 	done
-	echo $last
+	echo ${last}
 }
 
 # get the script root path
-DIR="$( cd `dirname "$0"` && pwd )"
+DIR="$( cd $(dirname "${0}") && pwd )"
 
 # verify $1
-if [ "$1" == "mac" -o "$1" == "unix" ]; then
+if [ "${1}" == "mac" -o "${1}" == "unix" ]; then
 
 	# configure completion path and bash profile name depends on OS given in $1
-	if [ "$1" == "mac" ]; then
-		COMPLETION_PATH=`brew --prefix`/etc/bash_completion.d
+	if [ "${1}" == "mac" ]; then
+		COMPLETION_PATH=$(brew --prefix)/etc/bash_completion.d
 		BASHFILE="bash_profile"
 	else
 		COMPLETION_PATH=/etc/bash_completion.d
@@ -25,24 +25,24 @@ if [ "$1" == "mac" -o "$1" == "unix" ]; then
 
 	# list
 	DOTFILES=(
-	$BASHFILE
+	${BASHFILE}
 	vim
 	vimrc
 	gitconfig
 	)
 
 	# uninstall 
-	if [ "$2" == "-u" ]; then
+	if [ "${2}" == "-u" ]; then
 		for file in ${DOTFILES[@]}; do
 			# remove link dotfiles when it's a symbolic link
-			if [ -h ~/."$file" -a $(lastPiece "`ls -ltr ~/.$file`") == $(echo "$DIR"/"$file") ]; then
-				rm ~/."$file"
+			if [ -h ~/."${file}" -a $(lastPiece "$(ls -ltr ~/.${file})") == $(echo "${DIR}"/"${file}") ]; then
+				rm ~/."${file}"
 			fi
 		done
-		if [ -d "$COMPLETION_PATH" ]; then
-			for file in `ls $DIR/bash_completion`; do
-				if [ -h "$COMPLETION_PATH"/"$file" -a $(lastPiece "`ls -ltr $COMPLETION_PATH/$file`") == $(echo "$DIR"/bash_completion/"$file") ]; then
-					sudo rm "$COMPLETION_PATH"/"$file"
+		if [ -d "${COMPLETION_PATH}" ]; then
+			for file in $(ls ${DIR}/bash_completion); do
+				if [ -h "${COMPLETION_PATH}"/"${file}" -a $(lastPiece "$(ls -ltr ${COMPLETION_PATH}/${file})") == $(echo "${DIR}"/bash_completion/"${file}") ]; then
+					sudo rm "${COMPLETION_PATH}"/"${file}"
 				fi
 			done
 		fi
@@ -50,20 +50,20 @@ if [ "$1" == "mac" -o "$1" == "unix" ]; then
 	else
 		# link dotfiles
 		for file in ${DOTFILES[@]}; do
-			if [ ! -h ~/."$file" -a ! -f ~/."$file" ]; then
-				ln -s "$DIR"/$file ~/.$file
+			if [ ! -h ~/."${file}" -a ! -f ~/."${file}" ]; then
+				ln -s "${DIR}"/${file} ~/.${file}
 			fi
 		done
 		# link completions
-		if [ -d "$COMPLETION_PATH" ]; then
-			for file in `ls $DIR/bash_completion`; do
-				if [ ! -h "$COMPLETION_PATH"/"$file" -a ! -f "$COMPLETION_PATH"/"$file" ]; then
-					sudo ln -s "$DIR"/bash_completion/"$file" "$COMPLETION_PATH"/"$file"
+		if [ -d "${COMPLETION_PATH}" ]; then
+			for file in $(ls ${DIR}/bash_completion); do
+				if [ ! -h "${COMPLETION_PATH}"/"${file}" -a ! -f "${COMPLETION_PATH}"/"${file}" ]; then
+					sudo ln -s "${DIR}"/bash_completion/"${file}" "${COMPLETION_PATH}"/"${file}"
 				fi
 			done
 		fi
 		# initial vim plugins
-		cd "$DIR" && git submodule init
-		cd "$DIR" && git submodule update
+		cd "${DIR}" && git submodule init
+		cd "${DIR}" && git submodule update
 	fi
 fi
